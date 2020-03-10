bac83e92308389d2b21700d064377898f2571a3f
Doug Klink <dougdklink3@gmail.com>
2020-03-10 13:03:23 +1300

    added oscillating vertical movement

--- dispersal model/animateParticles.m
+++ dispersal model/animateParticles.m
@@ -1,4 +1,5 @@
-file = '../output/parts_2015_2015_3d_500m.nc';
+filename = 'parts_2015_2015_3d_oscillating.nc';
+file = ['../output/' filename];
 lon = ncread(file, 'lon');
 lat = ncread(file, 'lat');
 releaseDate = ncread(file, 'releaseDate');
@@ -10,13 +11,30 @@ m_proj('miller');
 lon(lon > 180) = lon(lon > 180) - 360;
 [X, Y] = m_ll2xy(lon, lat);
 time = datenum(2015, 01, 01):datenum(0, 0, 1):datenum(2015, 01, ndays);
-for i=1:length(time)
+max_depth = max(depth, [], 'all');
+for i=1:2:length(time)
+    p1 = subplot(2, 1, 1);
     released = releaseDate < time(i);
     scatter(X(i, released), Y(i, released), 8, depth(i, released), 'filled');
-    colorbar();
-    colormap('cool');
+    cbar = colorbar();
+    set(cbar, 'ydir', 'reverse');
+    ylabel(cbar, 'Particle Depth (m)');
+    caxis([0, max_depth]);
+    colormap(flipud(colormap('parula')));
     m_coast();
     m_grid();
-    pause(.01);
+    
+    p2 = subplot(2, 1, 2);
+    xline(time(i), 'LineWidth', 5);
+    xlim([time(1), time(end)]);
+    datetick('x','keeplimits','keepticks')
+    xlabel('Current Time');
+    set(p2,'Ytick',[]);
+    
+    sgtitle(['Output file ' filename], 'Interpreter', 'none');
+
+    p1.Position = p1.Position + [-.18 -.4 .4 .4];
+    p2.Position = p2.Position - [0 0 0 .3];
+    pause(.0001);
 end
 
--- dispersal model/getVerticalTransport.m
+++ dispersal model/getVerticalTransport.m
@@ -11,6 +11,20 @@ function dz = getVerticalTransport(p, dt, settings)
     dz = dzdt*dt;
     %}
 
+    %{
     % mechanism: force to a specific depth
     dz = settings.forcedDepth - p.z;  % jump particles straight to forced_depth
+    %}
+    
+    %{
+    % mechanism: sin wave sinking/resurfacing to forced depth
+    current_time = datetime(settings.date, 'ConvertFrom', 'datenum');
+    start_time = datetime(settings.initDate, 'ConvertFrom', 'datenum');
+    period = 28; % days
+    mean_depth = 500;  %m
+    amplitude = 500; %m
+    z_new = mean_depth + amplitude*sin(2*pi* days(current_time-start_time)/period);
+    dz = z_new - p.z;  % jump particles to their spot on the sin wave
+    %}
+    
 end
